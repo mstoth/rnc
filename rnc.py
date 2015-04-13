028695bfe9ede35a7a7b22d882495d8bda100543
Michael Toth <michael@virtualpianist.com>
2015-04-13 18:37:17 -0400

    complete

--- rnc.py
+++ rnc.py
@@ -1,13 +1,23 @@
 # RNC or Roman Numeral Calculator
 # Written by Michael Toth
+    
+ROMAN_NUMERALS = ('I', 'V', 'X')
 
 def add(augend, addend):
     if not isinstance(augend, basestring) or not isinstance(addend, basestring):
         raise ValueError
-    simple_sum = augend + addend
-    if any(char != 'I' for char in simple_sum):
-        raise ValueError
-    return simple_sum
 
-
+    simple_augend = augend.replace("IX","IIIIIIIII").replace("IV","IIII")
+    simple_addend = addend.replace("IX","IIIIIIIII").replace("IV","IIII")
     
+    simple_sum = simple_augend + simple_addend
+    
+    if any(char not in ROMAN_NUMERALS for char in simple_sum):
+        raise ValueError
+
+    ordered_sum = ''.join(reversed(sorted(simple_sum)))
+    
+    canonicalised_sum = ordered_sum.replace('IIIII', 'V').\
+                        replace('IIII', 'IV').replace('VV', 'X').replace('VIV', 'IX')
+    
+    return canonicalised_sum
