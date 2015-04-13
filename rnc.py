abe3237bc453e494664ec29ddf3f96a735683120
Michael Toth <michael@virtualpianist.com>
2015-04-13 16:02:50 -0400

    more added

--- rnc.py
+++ rnc.py
@@ -2,9 +2,12 @@
 # Written by Michael Toth
 
 def add(augend, addend):
-    if not isinstance(addend, basestring):
+    if not isinstance(augend, basestring) or not isinstance(addend, basestring):
         raise ValueError
-    if addend == 'V':
+    simple_sum = augend + addend
+    if any(char != 'I' for char in simple_sum):
         raise ValueError
-    return augend + addend
+    return simple_sum
+
+
     
--- rncTest.py
+++ rncTest.py
@@ -1,17 +1,33 @@
 import unittest
 from rnc import add
 
+
 class AdditionTest(unittest.TestCase):
 
     def test_adding_Is(self):
         self.assertEqual(add('I', 'I'), 'II')
         self.assertEqual(add('I', 'II'), 'III')
 
+
     def test_inputs_out_of_scope_raise_exceptions(self):
-        with self.assertRaises(ValueError):
-            add('I', 2)
-        with self.assertRaises(ValueError):
-            add('I', 'V')
+        for bad_input in (2, None, 'Z', 'V', 'X', 'L', 'C', 'D', 'M'):
+            with self.assertRaises(ValueError) as m:
+                add('I', bad_input)
+                if not hasattr(m, 'exception'):
+                    self.fail('%s as augend did not raise exception' % bad_input)
+
+            with self.assertRaises(ValueError) as m:
+                add(bad_input, 'I')
+                if not hasattr(m, 'exception'):
+                    self.fail('%s as addend did not raise exception' % bad_input)
+
+    def test_IV_and_V(self):
+        self.assertEqual(add('II', 'II'), 'IV')
+        self.assertEqual(add('III', 'II'), 'V')
+        self.assertEqual(add('IV', 'I'), 'V')
+        self.assertEqual(add('V', 'I'), 'VI')
+        self.assertEqual(add('I', 'V'), 'VI')
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
